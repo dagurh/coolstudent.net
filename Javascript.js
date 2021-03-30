105c12f0d2c324f4ca4ce894897729b6b8de4998
Dagur Hardarson <201810188@post.au.dk>
2021-03-30 23:54:45 +0200

    side buttons to courses, chat drpdwn, css fixeslec

--- Javascript.js
+++ Javascript.js
@@ -4,47 +4,47 @@ toggle between hiding and showing the dropdown content */
 /* exported addGroup */
 
 function addGroup () {
-  document.getElementById('myDropdown').classList.toggle('show')
+  document.getElementById('myDropdown').classList.toggle('show');
 }
 
 function settings () {
-  document.getElementById('myDropdown2').classList.toggle('show2')
+  document.getElementById('myDropdown2').classList.toggle('show2');
 }
 
 function addChat () {
-  document.getElementById('myDropleft').classList.toggle('show3')
+  document.getElementById('myDropdown3').classList.toggle('show3');
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function (event) {
   if (!event.target.matches('.button1')) {
-    var dropdowns = document.getElementsByClassName('dropdown-content')
-    var i
+    const dropdowns = document.getElementsByClassName('dropdown-content');
+    let i;
     for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i]
+      const openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show')
+        openDropdown.classList.remove('show');
       }
     }
   }
   if (!event.target.matches('.button6')) {
-    var dropdowns2 = document.getElementsByClassName('dropdown-content2')
-    var j
+    const dropdowns2 = document.getElementsByClassName('dropdown-content2');
+    let j;
     for (j = 0; j < dropdowns2.length; j++) {
-      var openDropdown2 = dropdowns2[j]
+      const openDropdown2 = dropdowns2[j];
       if (openDropdown2.classList.contains('show2')) {
-        openDropdown2.classList.remove('show2')
+        openDropdown2.classList.remove('show2');
       }
     }
   }
   if (!event.target.matches('.ChatButton')) {
-    var droplefts = document.getElementsByClassName('dropleft-content')
-    var k
-    for (k = 0; j < droplefts.length; k++) {
-      var openDropleft = droplefts[k]
+    const droplefts = document.getElementsByClassName('dropdown-content3');
+    let k;
+    for (k = 0; k < droplefts.length; k++) {
+      const openDropleft = droplefts[k];
       if (openDropleft.classList.contains('show3')) {
-        openDropleft.classList.remove('show3')
+        openDropleft.classList.remove('show3');
       }
     }
   }
-}
+};
