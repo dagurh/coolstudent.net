b04f9d5d5647570deb9ace5b832973e23789d69b
Line Pernille Graulund <1558-au634047@users.noreply.gitlab.au.dk>
2021-04-15 12:57:18 +0200

    pop-up window added

--- Javascript.js
+++ Javascript.js
@@ -3,15 +3,15 @@ toggle between hiding and showing the dropdown content */
 
 /* exported addGroup */
 
-function addGroup () {
+function addGroup() {
   document.getElementById('myDropdown').classList.toggle('show');
 }
 
-function settings () {
+function settings() {
   document.getElementById('myDropdown2').classList.toggle('show2');
 }
 
-function addChat () {
+function addChat() {
   document.getElementById('myDropdown3').classList.toggle('show3');
 }
 
@@ -47,4 +47,57 @@ window.onclick = function (event) {
       }
     }
   }
-};
+}
+// Get the modal
+var protomodal = document.getElementById("protomodal");
+var conmodal = document.getElementById("conmodal");
+var normmodal = document.getElementById("normmodal");
+
+// Get the button that opens the modal
+var protobtn = document.getElementById("protobutton");
+var conbtn = document.getElementById("conbutton");
+var normbtn = document.getElementById("normbutton");
+
+// Get the <span> element that closes the modal
+var protospan = document.getElementsByClassName("close")[0];
+var conspan = document.getElementsByClassName("close")[1];
+var normspan = document.getElementsByClassName("close")[2];
+
+// When the user clicks the button, open the modal 
+protobtn.onclick = function () {
+  protomodal.style.display = "block";
+}
+conbtn.onclick = function () {
+  conmodal.style.display = "block";
+}
+normbtn.onclick = function () {
+  normmodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+protospan.onclick = function () {
+  protomodal.style.display = "none";
+}
+conspan.onclick = function () {
+  conmodal.style.display = "none";
+}
+normspan.onclick = function () {
+  normmodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == protomodal) {
+    protomodal.style.display = "none";
+  }
+}
+window.onclick = function (event) {
+  if (event.target == conmodal) {
+    conmodal.style.display = "none";
+  }
+}
+window.onclick = function (event) {
+  if (event.target == normmodal) {
+    normmodal.style.display = "none";
+  }
+}
