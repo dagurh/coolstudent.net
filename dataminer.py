33aebee595b5219cd1095429dccfb416e9f293af
au602716 <au602716@uni.au.dk>
2021-04-22 15:49:32 +0200

    adf

--- dataminer.py
+++ dataminer.py
@@ -1,12 +1,16 @@
 import sqlite3
 from typing import Iterable, List
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 import data as db
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+  return redirect('https://vm06.exsys2021.cs.au.dk/index')
+
+@app.route('/index')
+def indexx():
   return render_template('index.html', list=db.all_deadlines())
 
 @app.route('/kalender')
