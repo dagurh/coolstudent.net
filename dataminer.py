0775121ee089b3e0e3e097f96c2140bfe6523c01
au602716 <au602716@uni.au.dk>
2021-04-22 14:44:33 +0200

    kalender added to app.route

--- dataminer.py
+++ dataminer.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def index():
   return render_template('index.html', list=db.all_deadlines())
 
+@app.route('/kalender')
+def kalender():
+  return render_template('kalender.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
 
