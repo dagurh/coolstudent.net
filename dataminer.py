52041c8851e371fdcd583bd8334fb83cf0dc4f66
Blue <201908416@post.au.dk>
2021-05-03 08:48:50 +0200

    m

--- dataminer.py
+++ dataminer.py
@@ -9,6 +9,18 @@ app = Flask(__name__)
 def index():
   return render_template('index.html', list=db.all_deadlines(), exsyslit=db.exsysLit(), comarklit=db.comarkLit(), statlit=db.statLit(), exsystø=db.exsysTØ(), comarktø=db.comarkTØ(), stattø=db.statTØ(), exsyslec=db.exsysLec(), comarklec=db.comarkLec(), statlec=db.statLec())
 
+@app.route('/Exsys')
+def Exsys():
+  return render_template('Exsys.html')
+
+@app.route('/ComArk')
+def Comark():
+  return render_template('ComArk.html')
+
+@app.route('/Stat')
+def Stat():
+  return render_template('Stat.html')
+
 @app.route('/kalender')
 def kalender():
   return render_template('kalender.html')
